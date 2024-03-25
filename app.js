160bfac4131d27b5a73fd422496cee940bd0557a
夏沫花火zzz <xmcn@1l1.icu>
2024-03-26 00:12:10 +0800

    更新：优化结构，添加README (#1)
    * 更新：优化结构，添加README
    
    * README添加换行
    
    * Add files via upload
    
    * response 404 when not found
    
    * Add files via upload
    
    * Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,62 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const getter = require("./src/get");
 
 const app = express();
 
-// 读取 yulu.txt 文件并将每行内容存储到一个数组中
-const yuluData = fs.readFileSync(path.join(__dirname, 'yulu.txt'), 'utf8').split('\n');
-
-// 处理 GET 请求 /yiyan/txt
-app.get('/yiyan/txt', (req, res) => {
-  // 从 yuluData 数组中随机选择一行
-  const randomLine = yuluData[Math.floor(Math.random() * yuluData.length)];
-  res.send(randomLine);
+// 处理 GET 请求 /yiyan/text
+app.get("/yiyan/text", (req, res) => {
+  res.send(getter.text());
 });
 
 // 处理 GET 请求 /yiyan/json
-app.get('/yiyan/json', (req, res) => {
-  // 从 yuluData 数组中随机选择一行
-  const randomLine = yuluData[Math.floor(Math.random() * yuluData.length)];
+app.get("/yiyan/json", (req, res) => {
   // 将内容包装为 RESTful API 格式
-  const data = { data: randomLine };
+  const data = {
+    status: 200,
+    message: "OK",
+    data: getter.text(true),
+  };
   res.json(data);
 });
 
 // 处理 GET 请求 /yiyan/img
-app.get('/yiyan/img', (req, res) => {
-  // 获取 imgs 文件夹下的所有文件
-  fs.readdir(path.join(__dirname, 'imgs'), (err, files) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Internal Server Error');
-    }
-    // 从文件列表中随机选择一张图片文件
-    const randomFile = files[Math.floor(Math.random() * files.length)];
-    // 返回选中的图片文件
-    res.sendFile(path.join(__dirname, 'imgs', randomFile));
-    // 加入headers使图片直接显示
-    res.set('Content-Type', 'image/jpeg');
-  });
+app.get("/yiyan/img", async (req, res) => {
+  // 返回选中的图片文件
+  res.sendFile(path.join(__dirname, "./resources/imgs", await getter.img()));
+  // 加入headers使图片直接显示
+  res.set("Content-Type", "image/jpeg");
 });
 // 处理 default 请求
-app.get('/', (req, res) => {
-  res.send('Welcome to dyc API');
+app.get("/", (req, res) => {
+  const data = {
+    status: 200,
+    message: "Welcome to dyc API, version 1x.",
+    data: {},
+  };
+  res.json(data);
+});
+app.all("*", (req, res) => {
+  const data = {
+    status: 404,
+    message: "Not found.",
+    data: {},
+  };
+  res.status(404);
+  res.json(data);
 });
 
 // 监听端口 3000
 app.listen(3000, () => {
-    // 全局加入跨域
-    app.all('*', function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-      res.header("X-Powered-By",' 3.2.1')
-      res.header("Content-Type", "application/json;charset=utf-8");
-      next();
-    });
-  console.log('Server is running on port 3000');
+  // 全局加入跨域
+  app.all("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.header("X-Powered-By", " 3.2.1");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+  });
+  console.log("Server is running on port 3000");
 });
